055021579e50ce541e9918a5458b7eca0d32507d
Davi-Arauj <moreiradaavi@gmail.com>
2022-09-13 08:55:57 -0300

    testando o OlaMundo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module aprendaGoTDD
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+const prefixoOlaPortugues = "Olá, "
+
+func Ola(nome string) string {
+	if nome == "" {
+		nome = "Mundo"
+	}
+
+	return prefixoOlaPortugues + nome
+
+}
+func main() {
+}
